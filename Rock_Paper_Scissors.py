c4409570ab3ece100400bbee3a73bca7c4217a8d
Rafdegus <106196710+Rafdegus@users.noreply.github.com>
2023-10-17 15:10:32 +0100

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,14 +1,18 @@
-# Jogo "Pedra Papel Tesoura" V:0.0 #
+# Jogo "Pedra Papel Tesoura" V:1.0 #
 # Coder: Rafael Gusmão #
 
 import random
 
-# 01 Instanciar variáveis #
-Escolha_Jogador = input("Escolhe um (pedra, papel, tesoura): ").lower()
-pc = random.choice(("pedra", "papel", "tesoura"))
+
 
 # 02 Lógica #
 def escolha():
+    global Escolha_Jogador
+    global pc
+    
+    Escolha_Jogador = input("Escolhe um (pedra, papel, tesoura): ").lower()
+    pc = random.choice(("pedra", "papel", "tesoura"))
+    
     if Escolha_Jogador == "pedra" and pc == "tesoura":
         return "Jogador Ganhou"
     if Escolha_Jogador == "pedra" and pc == "papel":
@@ -27,17 +31,24 @@ def escolha():
         return "Jogador Ganhou"
     if Escolha_Jogador == "tesoura" and pc == "tesoura":
         return "Jogador Empatou"
+    if Escolha_Jogador == "exit":
+        return "exit"
     return ""
 
-resultado = escolha()
 
+# Loop infinito, apenas sai se resultado = exit #
 while True:
-    if resultado != "":
-        break
-    print("Escolha Errada")
-    Escolha_Jogador = input("Escolhe um (pedra, papel, tesoura): ").lower()
+    # Chamada da função #
     resultado = escolha()
+    if resultado != "":
+        if resultado == "exit":
+            # sai do loop (termina programa) #
+            break
+        # resultado válido #
+        print(resultado)
+        print("Jogador:", Escolha_Jogador, "| PC:", pc)
+    else:
+        # resultado inválido #
+        print("Escolha Errada")
 
-# 03 Resultado #
-print(resultado)
-print("Jogador:", Escolha_Jogador, "| PC:", pc)
+
